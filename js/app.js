8f9db81091db1fd06bf331a245b8db207bbe3a9c
Raquel Ramos <raquelramos6t@gmail.com>
2024-02-04 18:03:59 -0300

    projeto amigo secreto

---
+++ js/app.js
@@ -0,0 +1,67 @@
+//recuperar os nomes;
+let nomesLista = [];
+function adicionar(){
+    let nomes = document.getElementById('nome-amigo').value;
+    let nomeMaiusculo = transformaParaMaiusculo(nomes);
+    if (nomes != '') {
+        if (nomesLista.includes(nomeMaiusculo)) {
+            document.getElementById('nome-amigo').value = '';
+            alert('Esse amigo já está incluso');
+            return;
+        }
+        nomesLista.push(nomeMaiusculo);
+        adicionarNomesLista();
+        document.getElementById('nome-amigo').value = '';
+    } else{
+        alert('Insira um nome do amigo(a)');
+        return;
+    }
+}
+
+function transformaParaMaiusculo(palavra) {
+    return palavra.toUpperCase();
+}
+
+//adicionar o nome a lista;
+function adicionarNomesLista(){
+    let listaAmigos = document.getElementById('lista-amigos');
+    listaAmigos.innerHTML = nomesLista.join(',');
+}
+//sortear os nomes;
+function sortear(){
+    if(nomesLista.length < 4){
+        alert('Insira pelo menos 4 participantes');
+        return;
+    }
+    const nomeSorteado = embaralha(nomesLista);
+    for(let i =0; i < nomesLista.length; i++){
+        let sorteios = document.getElementById('lista-sorteio');
+        if(i == nomesLista.length - 1){
+            sorteios.innerHTML = sorteios.innerHTML +  nomesLista[i] + ' -> ' +  nomesLista[0] + '<br>';
+        } else{
+            sorteios.innerHTML = sorteios.innerHTML +  nomesLista[i] + ' -> ' +  nomesLista[i + 1] + '<br>';
+        }
+        
+    }
+
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+
+//reninciar;
+function reiniciar(){
+    nomesLista = [];
+    document.getElementById('lista-sorteio').innerHTML = '';
+    document.getElementById('lista-amigos').innerHTML = '';
+}
